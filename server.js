47707086cec3d923277361e52787129ebdec793e
prithvipsingh <ppsdeora@gmail.com>
2020-06-14 16:37:42 +1000

    added express pacakage

--- server.js
+++ server.js
@@ -6,17 +6,17 @@ const PORT = process.env.PORT || 3000;
 
 
 const app = express();
-app.use(logger("dev"));
+//app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-const db = require("./models");
+const db = require("./Develop/models");
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./Develop/routes/apiRoutes")(app);
+require("./Develop/routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
